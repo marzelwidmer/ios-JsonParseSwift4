deaf31c340c4d1af1f62ce06484bdbf888485b0b
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-28 10:52:31 +0200

    comment API URL's

--- JsonParseSwift4/ViewController.swift
+++ JsonParseSwift4/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
-
+// https://api.letsbuildthatapp.com/jsondecodable/course
+// https://api.letsbuildthatapp.com/jsondecodable/courses
+// https://api.letsbuildthatapp.com/jsondecodable/website_description
+// https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
