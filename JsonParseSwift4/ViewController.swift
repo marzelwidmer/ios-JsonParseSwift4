3436c230b2e6b175f53a1bb7a3d52ab6ee5a33c1
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-28 16:28:56 +0200

    refactor add complicated json parser remove swift 2/3/code

--- JsonParseSwift4/ViewController.swift
+++ JsonParseSwift4/ViewController.swift
@@ -11,28 +11,19 @@
 
 import UIKit
 
-
+// Model Object
+struct WebsiteDescription: Decodable {
+    let name: String?
+    let description: String?
+    let courses: [Course]?
+}
 
 // Model Object
-//{
-//    "id": 1,
-//    "name": "Instagram Firebase",
-//    "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
-//    "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93",
-//    "number_of_lessons": 49
-//}
 struct Course: Decodable {
-    let id: Int
-    let name: String
-    let link: String
-    let imageUrl: String
-
-//    init(json: [String: Any]) {
-//        id = json["id"] as? Int ?? -1
-//        name = json["name"] as? String ?? ""
-//        link = json["link"] as? String ?? ""
-//        imageUrl = json["imageUrl"] as? String ?? ""
-//    }
+    let id: Int?
+    let name: String?
+    let link: String?
+    let imageUrl: String?
 }
 
 class ViewController: UIViewController {
@@ -40,37 +31,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/course"
-        let url = URL(string: jsonUrlString)
-        
-        URLSession.shared.dataTask(with: url!) { (data, response, err) in
-            // perhaps checkk err
-            // also perhaps check response status 200 OK
+        let urlCourse = URL(string: "https://api.letsbuildthatapp.com/jsondecodable/course")
+        URLSession.shared.dataTask(with: urlCourse!) { (data, response, err) in
             guard let data = data else {return}
-            
-            //            let dataAsString = String(data: data, encoding: .utf8)
-            //            print(dataAsString)
-            
             do{
                 let course = try
                     JSONDecoder().decode(Course.self, from: data)
-                print(course.name)
-                
-                
-//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-//                    as? [String: Any] else {return}
-//                let course = Course(json: json)
-//                print(course.name)
-    
+                print(course.name!)
             } catch let  jsonErr{
                 print("Error serializing json:" , jsonErr)
             }
-            
             }.resume()
         
         
-        //        let myCourse = Course(id: 1, name: "myCourse", link: "myLink", imageUrl: "some  URL")
-        //        print(myCourse)
+        
+        let urlCourses = URL(string: "https://api.letsbuildthatapp.com/jsondecodable/courses")
+        URLSession.shared.dataTask(with: urlCourses!) { (data, response, err) in
+            guard let data = data else {return}
+            do{
+                let courses = try
+                    JSONDecoder().decode([Course].self, from: data)
+                print(courses)
+                
+            } catch let  jsonErr{
+                print("Error serializing json:" , jsonErr)
+            }
+            }.resume()
+        
+        
+        
+        let urlWebsiteDescription = URL(string: "https://api.letsbuildthatapp.com/jsondecodable/website_description")
+        URLSession.shared.dataTask(with: urlWebsiteDescription!) { (data, response, err) in
+            guard let data = data else {return}
+            do{
+                let websiteDescription = try
+                    JSONDecoder().decode(WebsiteDescription.self, from: data)
+                print(websiteDescription.name!)
+                print(websiteDescription.description!)
+                
+            } catch let  jsonErr{
+                print("Error serializing json:" , jsonErr)
+            }
+            }.resume()
     }
 }
+
 
